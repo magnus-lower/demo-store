13f54d845595b61b6df211439ef3e33374c7bc33
Magnus Løwer <magnus.lower@gmail.com>
2024-08-05 21:18:03 +0200

    Fixed typo error in ProductController

--- src/main/java/com/example/ecommerce/controller/ProductController.java
+++ src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -14,8 +14,8 @@ public class ProductController {
     public List<Product> getProducts() {
         List<Product> products = new ArrayList<>();
         products.add(new Product(1L, "Flaske", "En drikkeflaske som kan brukes til alle formål", 10.99, "/images/Bottle.jpg", "Mat og drikke"));
-        products.add(new Product(2L, "Solbriller", "Solbriller som passer perfekt til alle", 12.99, "/images/sunglasses.jpg", "Klær"));
-        products.add(new Product(3L, "Potteplante", "En potteplante som passer til alle typer planter", 15.99, "/images/plant pot.jpg", "Møbler"));
+        products.add(new Product(2L, "Solbriller", "Solbriller som passer perfekt til alle", 12.99, "/images/Sunglasses.jpg", "Klær"));
+        products.add(new Product(3L, "Potteplante", "En potteplante som passer til alle typer planter", 15.99, "/images/Plant pot.jpg", "Møbler"));
         return products;
     }
 }
